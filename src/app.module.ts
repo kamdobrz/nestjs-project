0d7ddf097122ce3f9ad7784b70e10a9c9d402e6c
Kamil Dobrzyński <kamil.dobrzynski@neoteric.eu>
2020-10-04 15:21:56 +0200

    feat(): add mongoose init configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,17 @@ import {Module} from '@nestjs/common';
 import {AuthModule} from './auth/auth.module';
 import {UsersModule} from './users/users.module';
 import {VideosModule} from './videos/videos.module';
+import {MongooseModule} from '@nestjs/mongoose';
 
 @Module({
-    imports: [VideosModule, AuthModule, UsersModule]
+    imports: [
+        VideosModule,
+        AuthModule,
+        UsersModule,
+        MongooseModule.forRoot(
+            `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}<dbname>?retryWrites=true&w=majority`
+        )
+    ]
 })
+
 export class AppModule {}
