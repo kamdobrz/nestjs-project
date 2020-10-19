be6a9c89fc6214083cdd6f118c7f6a86f244cd6a
Kamil Dobrzyński <kamil.dobrzynski@neoteric.eu>
2020-10-19 23:05:57 +0200

    feat(user-schema): create users in mongo

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,30 +1,33 @@
-import {Injectable} from '@nestjs/common';
+import {HttpException, HttpStatus, Injectable} from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 import {AuthCredentialsDto} from '../dto/users/user.dto';
-import {UserInterface} from '../shared/interfaces/user.interface';
+import {InjectModel} from '@nestjs/mongoose';
+import {User, UserDocument} from '../schemas/user.schema';
+import {Model} from 'mongoose';
 
 @Injectable()
 export class UsersService {
-    private users: UserInterface[] = [];
+    constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
-    public create = async({username, password}: AuthCredentialsDto): Promise<boolean> => {
-        const user = this.findOne(username);
+    async create({username, password}: AuthCredentialsDto): Promise<User> {
+        const user = await this.findOne(username);
+
         if (user) {
-            return false;
+            throw new HttpException('User exists!', HttpStatus.FORBIDDEN);
         }
 
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
 
-        this.users.push({
+        const createdUser = new this.userModel({
             id: Date.now().toString(),
             password: hashedPassword,
             username
         });
 
-        return true;
-    };
+        return createdUser.save();
+    }
 
-    public findOne = (username: string): UserInterface =>
-        this.users.find((user: UserInterface) => user.username === username);
+    public findOne = async(username: string): Promise<UserDocument> =>
+        await this.userModel.findOne({username});
 }
